be41cae1ed87b25baca908dd64955b3de076f4fa
Stephane <schemansteph@gmail.com>
2026-06-09 05:15:50 +0200

    offline back

--- components/ServiceWorkerRegister.tsx
+++ components/ServiceWorkerRegister.tsx
@@ -1,31 +1,20 @@
 'use client'
+
 import { useEffect } from 'react'
+import { syncManager } from '@/lib/syncManager'
 
 export function ServiceWorkerRegister() {
   useEffect(() => {
-    if (!('serviceWorker' in navigator)) return
-    navigator.serviceWorker.register('/sw.js', { scope: '/' })
-      .then(reg => {
-        setInterval(() => reg.update(), 60 * 1000)
-        reg.addEventListener('updatefound', () => {
-          const newWorker = reg.installing
-          if (!newWorker) return
-          newWorker.addEventListener('statechange', () => {
-            if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-              newWorker.postMessage('SKIP_WAITING')
-            }
-          })
-        })
-      })
-      .catch(err => console.warn('[SW] Erreur:', err))
+    // Démarrer la sync auto
+    const stopSync = syncManager.startAutoSync()
 
-    let refreshing = false
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      if (!refreshing) {
-        refreshing = true
-        console.log('[SW] Nouveau SW actif')
-      }
-    })
+    // Enregistrer le Service Worker
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {})
+    }
+
+    return () => { stopSync?.() }
   }, [])
+
   return null
 }
